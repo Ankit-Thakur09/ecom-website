47b0f638967942bce96267e9bb0a1d610a77210e
Ankit-Thakur09 <ankitthakur12499@gmail.com>
2025-03-19 23:45:40 +0530

    contact form initiated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+import { motion } from 'framer-motion'
+import emailjs from "@emailjs/browser"
+import {styles} from "../style"
+import {SectionWrapper} from "../../hoc/index"
+import { slideIn } from "../../utils/motion"
+import {EarthCanvas} from "../../components/canvas/index"
+
 
 const Contact = () => {
+    const formRef = useRef();
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message:""
+    })
+    const [loading, setLoading] = useState(false)
+    const handleChange = (e) => { }
+     const handleSubmit=(e)=>{}
+
   return (
-    <div>page</div>
+      <div className='xl:mt-12 xl:flex-row flex flex-col-reverse gap-10 overflow-hidden'>
+          <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] p-8 rounded-2xl'></motion.div>
+          <p>Get in touch</p>
+          h3
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
